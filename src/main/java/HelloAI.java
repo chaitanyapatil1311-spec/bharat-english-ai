4fad126fd02b418f3b4a0240e566af8f0517a7c0
chaitanyapatil1311-spec <chaitanyapatil1311@gmail.com>
2026-09-04 15:58:51 +0530

    Update greeting message in HelloAI

--- src/main/java/HelloAI.java
+++ src/main/java/HelloAI.java
@@ -1,7 +1,7 @@
 public class HelloAI {
 
     public static void main(String[] args) {
-	System.out.println("Hello! We are` your Bharat English AI Assistant.");
+	System.out.println("Hello! We are going to be  your Bharat English AI Assistant.");
 	System.out.println("English Tip: Use 'I am going to...' when talking about a planned action.");
     }
 }
