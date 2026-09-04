1857526e14d98b2f0f195f25a07023d8d727bf9e
Chaitanya Patil <chaitanyapatil1311@gmail.com>
2026-09-04 16:18:52 +0530

    Revert "Temporary developer greeting"
    This reverts commit 7b861762630e127129283a3e1572d3f10887c80a.

--- src/main/java/HelloAI.java
+++ src/main/java/HelloAI.java
@@ -1,7 +1,7 @@
 public class HelloAI {
 
     public static void main(String[] args) {
-	System.out.println("Hello! I am your Bharat English AI Developer.");
+	System.out.println("Hello! We are going to be the your Bharat English AI Assistant.");
 	System.out.println("English Tip: Use 'I am going to...' when talking about a planned action.");
     }
 }
